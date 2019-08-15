f81b3256cb68efe6a3e64ba56a888c0b1ecd0ca8
Minggl <guangliang1120@gmail.com>
2019-08-15 09:03:03 +0800

    [TECH][glming] add heartbeat

--- src/main/java/com/github/mgljava/heartbeat/HeartbeatHandler.java
+++ src/main/java/com/github/mgljava/heartbeat/HeartbeatHandler.java
@@ -1,8 +1,15 @@
 package com.github.mgljava.heartbeat;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.util.CharsetUtil;
+import java.net.InetSocketAddress;
 
 public class HeartbeatHandler extends ChannelInboundHandlerAdapter {
 
@@ -24,6 +31,19 @@ public class HeartbeatHandler extends ChannelInboundHandlerAdapter {
           break;
       }
       System.out.println(ctx.channel().remoteAddress() + "超时事件：" + eventType);
+
+      final Channel channel = ctx.channel();
+      final ChannelFuture future = channel.connect(new InetSocketAddress("localhost", 8080));
+      future.addListener((ChannelFutureListener) future1 -> {
+        if (future1.isSuccess()) {
+          ByteBuf buffer = Unpooled.copiedBuffer("Hello", CharsetUtil.UTF_8);
+          future1.channel().writeAndFlush(buffer);
+        } else {
+          final Throwable cause = future1.cause();
+          cause.printStackTrace();
+        }
+      });
+
       ctx.channel().close();
     }
   }
