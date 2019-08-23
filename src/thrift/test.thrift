4ac1accba59f9507a3ae6cdbac16a1fb0d090142
Minggl <guangliang1120@gmail.com>
2019-08-23 15:03:43 +0800

    [TECH][glming] 生成 thrift 代码

--- src/thrift/test.thrift
+++ src/thrift/test.thrift
@@ -2,6 +2,19 @@ namespace java com.github.mgljava.thrift.server
 
 typedef i32 int
 
+struct News {
+  1:int id;
+  2:string title;
+  3:string content;
+  4:string mediaFrom;
+  5:string author;
+}
+
+service IndexNewsOperatorServices {
+  bool indexNews(1:int indexNews),
+  bool removeNewsById(1:int id)
+}
+
 service AddtionService {
   int add (1:int n1, 2:int n2),
 }
