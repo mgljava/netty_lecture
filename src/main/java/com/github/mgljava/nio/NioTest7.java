f042d82b9f31cf56579da94d46d4f5ea990ed975
Minggl <guangliang1120@gmail.com>
2019-09-10 17:42:28 +0800

    [TECH][glming] add nio test class

---
+++ src/main/java/com/github/mgljava/nio/NioTest7.java
@@ -0,0 +1,21 @@
+package com.github.mgljava.nio;
+
+import java.nio.ByteBuffer;
+
+/**
+ * 只读Buffer， 我们可以调用Buffer的asReadOnlyBuffer转为一个只读Buffer，
+ * 但是我们不能将只读Buffer转为可读写的Buffer
+ */
+public class NioTest7 {
+
+  public static void main(String[] args) {
+    ByteBuffer buffer = ByteBuffer.allocate(10);
+    System.out.println(buffer.getClass());
+    for (int i = 0; i < buffer.capacity(); i++) {
+      buffer.put((byte) i);
+    }
+    final ByteBuffer asReadOnlyBuffer = buffer.asReadOnlyBuffer();
+    System.out.println(asReadOnlyBuffer.getClass());
+
+  }
+}
