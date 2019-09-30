4c66eb04414ef75f1af82d0ebc09f216c02ca535
Minggl <guangliang1120@gmail.com>
2019-09-30 15:06:11 +0800

    [TECH][glming] add nio class

--- src/main/java/com/github/mgljava/nio/NioClient.java
+++ src/main/java/com/github/mgljava/nio/NioClient.java
@@ -1,10 +1,76 @@
 package com.github.mgljava.nio;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class NioClient {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    SocketChannel socketChannel = null;
+    try {
+      socketChannel = SocketChannel.open();
+      socketChannel.configureBlocking(false);
 
+      Selector selector = Selector.open();
+      socketChannel.register(selector, SelectionKey.OP_CONNECT);
+      socketChannel.connect(new InetSocketAddress("127.0.0.1", 8899));
 
+      while (true) {
 
+        selector.select();
+        final Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
+        selectionKeys.forEach(selectionKey -> {
+
+          if (selectionKey.isConnectable()) {
+            SocketChannel client = (SocketChannel) selectionKey.channel();
+
+            // 连接是否在进行
+            if (client.isConnectionPending()) {
+              try {
+                client.finishConnect();
+                ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+                writeBuffer.put((LocalDateTime.now() + " 连接成功").getBytes());
+
+                writeBuffer.flip();
+                client.write(writeBuffer);
+
+                // 当建立好连接之后，新开一个线程来处理控制台的输入数据
+                ExecutorService executorService = Executors.newSingleThreadExecutor(Executors.defaultThreadFactory());
+                executorService.submit(() -> {
+                  while (true) {
+                    writeBuffer.clear();
+                    InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+                    BufferedReader bf = new BufferedReader(inputStreamReader);
+                    final String sendMessage = bf.readLine();
+                    writeBuffer.put(sendMessage.getBytes());
+                    writeBuffer.flip();
+                    client.write(writeBuffer);
+                  }
+                });
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
+            }
+          }
+        });
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      if (null != socketChannel) {
+        socketChannel.close();
+      }
+    }
   }
 }
