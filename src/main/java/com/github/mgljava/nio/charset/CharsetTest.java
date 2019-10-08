8cb53e4e3864a746d7248be7fd451e854e902786
Minggl <guangliang1120@gmail.com>
2019-10-08 13:42:17 +0800

    [TECH][glming] add charset test

--- src/main/java/com/github/mgljava/nio/charset/CharsetTest.java
+++ src/main/java/com/github/mgljava/nio/charset/CharsetTest.java
@@ -1,10 +1,87 @@
 package com.github.mgljava.nio.charset;
 
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+
 public class CharsetTest {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
 
+    String inputFile = "niotestfile/CharsetTest_In.txt";
+    String outputFile = "niotestfile/CharsetTest_Out.txt";
 
+    RandomAccessFile inputRandomAccessFile = new RandomAccessFile(inputFile, "r");
+    RandomAccessFile outputRandomAccessFile = new RandomAccessFile(outputFile, "rw");
 
+    long inputFileLength = new File(inputFile).length();
+
+    final FileChannel inputFileChannel = inputRandomAccessFile.getChannel();
+    final FileChannel outputFileChannel = outputRandomAccessFile.getChannel();
+
+    final MappedByteBuffer inputData = inputFileChannel.map(MapMode.READ_ONLY, 0, inputFileLength);
+
+    // final Charset charset = Charset.forName("utf-8");
+    final Charset charset = Charset.forName("iso-8859-1");
+    final CharsetDecoder charsetDecoder = charset.newDecoder(); // 把字节数组转为字符串
+    final CharsetEncoder charsetEncoder = charset.newEncoder(); // 把字符串转为字节数组
+
+    Charset.availableCharsets().forEach((key, value) -> System.out.println(key + ", " + value));
+
+    System.out.println("\n");
+
+    System.out.println(Charset.defaultCharset());
+
+    CharBuffer charBuffer = charsetDecoder.decode(inputData);
+    ByteBuffer outputData = charsetEncoder.encode(charBuffer);
+
+    outputFileChannel.write(outputData);
+
+    inputRandomAccessFile.close();
+    outputRandomAccessFile.close();
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
