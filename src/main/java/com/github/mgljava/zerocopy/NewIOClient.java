11b6b8e2b2611ec7a840a31fc799eb603d9ecd82
Minggl <guangliang1120@gmail.com>
2019-10-14 17:08:06 +0800

    [TECH][glming] Nio 零拷贝

--- src/main/java/com/github/mgljava/zerocopy/NewIOClient.java
+++ src/main/java/com/github/mgljava/zerocopy/NewIOClient.java
@@ -15,10 +15,27 @@ public class NewIOClient {
 
     String fileName = "d:/VM/CentOS-7-x86_64-Minimal-1810.iso";
 
-    FileChannel fileChannel = new FileInputStream(fileName).getChannel();
-    long start = System.currentTimeMillis();
-    long transferCount = fileChannel.transferTo(0, fileChannel.size(), socketChannel);
-    System.out.println("发送的总字节数：" + transferCount + ", 耗时：" + (System.currentTimeMillis() - start));
-    fileChannel.close();
+    try (FileChannel fileChannel = new FileInputStream(fileName).getChannel()) {
+
+      long position = 0;
+      long size = fileChannel.size();
+      long total = 0;
+      int count = 0;
+      long start = System.currentTimeMillis();
+      while (position < size) {
+        long transferCount = fileChannel.transferTo(0, fileChannel.size(), socketChannel);
+        System.out.println("发送：" + transferCount);
+        if (transferCount < 0) {
+          break;
+        }
+        count++;
+        total += transferCount;
+        position += transferCount;
+      }
+
+      System.out.println("发送的总字节数：" + total + ", 耗时：" + (System.currentTimeMillis() - start) + ", 传输次数：" + count);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
