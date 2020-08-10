33812f768ba6846e30df787fa598b3e8ec97791a
Minggl <guangliang1120@gmail.com>
2020-08-10 23:34:20 +0800

    [TECH][Minggl]: echo code

---
+++ src/main/java/com/github/mgljava/echo/EchoServerInitializer.java
@@ -0,0 +1,18 @@
+package com.github.mgljava.echo;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class EchoServerInitializer extends ChannelInitializer<SocketChannel> {
+
+  @Override
+  protected void initChannel(SocketChannel ch) throws Exception {
+    ChannelPipeline pipeline = ch.pipeline();
+    pipeline.addLast(new StringEncoder());
+    pipeline.addLast(new StringDecoder());
+    pipeline.addLast(new EchoServerHandler());
+  }
+}
