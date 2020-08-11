a996a00f4591ed220902278872d4fc784a3ca76b
Minggl <guangliang1120@gmail.com>
2020-08-11 09:50:55 +0800

    [TECH][Minggl]: echo code

--- src/main/java/com/github/mgljava/echo/EchoServerHandler.java
+++ src/main/java/com/github/mgljava/echo/EchoServerHandler.java
@@ -1,8 +1,10 @@
 package com.github.mgljava.echo;
 
+import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+@Sharable // 标志一个ChannelHandler 可以被多个Channel安全的共享
 public class EchoServerHandler extends SimpleChannelInboundHandler<String> {
 
   @Override
