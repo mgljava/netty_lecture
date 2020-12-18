d9d278d3caef52c307ad69ee8bc6652ee45bbed1
Minggl <guangliang1120@gmail.com>
2020-12-18 09:44:01 +0800

    [TECH][Minggl]: refactor code

--- src/main/java/com/github/mgljava/secondexample/MyServer.java
+++ src/main/java/com/github/mgljava/secondexample/MyServer.java
@@ -12,12 +12,28 @@ public class MyServer {
 
     // 死循环,NioEventLoopGroup完成一些准备工作，给一些变量赋值
     EventLoopGroup bossGroup = new NioEventLoopGroup();
+
+    /*
+     * 可以设置 bossGroup的线程数为1
+     * EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+     */
     EventLoopGroup workerGroup = new NioEventLoopGroup();
 
     try {
       ServerBootstrap serverBootstrap = new ServerBootstrap();
-      serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+
+      /*
+      group(bossGroup, workerGroup) 作用是给成员变量赋值
+      channel(NioServerSocketChannel.class) 给channel的成员变量赋值
+      .childHandler(new MyServerInitializer()) 给父类的成员变量赋值childHandler
+       */
+      serverBootstrap.group(bossGroup, workerGroup)
+          .channel(NioServerSocketChannel.class)
           .childHandler(new MyServerInitializer());
+
+      /**
+       * bind 方法：创建一个新的channel进行绑定
+       */
       final ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
       channelFuture.channel().closeFuture().sync();
     } finally {
